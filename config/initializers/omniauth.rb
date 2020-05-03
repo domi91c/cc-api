50cf5ceae93b92093fe13c16ed799b506e42e606
Dominic Nunes <dominic.n@me.com>
2020-05-02 23:52:37 -0400

    wip

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,1 @@
-OmniAuth.config.full_host = Rails.env.staging? ? 'https://www.cuecast.io' : 'http://localhost:4000'
+# OmniAuth.config.full_host = Rails.env.staging? ? 'https://www.cuecast.io' : 'http://localhost:4000'
