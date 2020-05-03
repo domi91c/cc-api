cde738ee709761c2002b1245b67bdd588accd4b4
Dominic Nunes <dominic.n@me.com>
2020-05-02 22:47:52 -0400

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   default_url_options :host => "cuecast.io"
-
   mount_devise_token_auth_for 'User',
     at: 'api/auth',
     controllers: {
@@ -8,6 +7,7 @@ Rails.application.routes.draw do
         'overrides/omniauth_callbacks'
     }
   mount ActionCable.server => '/cable'
+
   scope :api do
     resources :streams do
       resources :requests, module: :streams
